6cd6dedb19367c6644cfa15ebc21b35737ad710b
enderta <endertanver@gmail.com>
2023-10-25 14:06:10 +0100

    Refactor database connection details to use environment variable

--- app.js
+++ app.js
@@ -19,11 +19,7 @@ const createApp = () => {
 
 const createPoolAndConnect = () => {
     const pool = new Pool({ // Database connection details moved to .env file
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_DATABASE,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
+        connectionString: process.env.DB_URL,
         ssl: {
             rejectUnauthorized: false,
         },
